cf99ab81113b1c8657a1a72a9f559b4835d13574
Unknown <david.b586@stu.nebo.edu>
2018-10-01 10:10:45 -0600

    fixed enemy so he moves

--- 2D Game/2Dgame/Assets/Scripts/EnemyPatrol.cs
+++ 2D Game/2Dgame/Assets/Scripts/EnemyPatrol.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour {
@@ -12,29 +11,30 @@ public class EnemyPatrol : MonoBehaviour {
 	public Transform WallCheck;
 	public float WallCheckRadius;
 	public LayerMask WhatIsWall;
-	private bool Hittingwall;
+	private bool HittingWall;
+
 	// Edge Check
 	private bool NotAtEdge;
 	public Transform EdgeCheck;
 
 
 	// Update is called once per frame
-	void update () {
+	void Update () {
 		NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
 		
-		Hittingwall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+		HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
 
-		if (Hittingwall || !NotAtEdge){
+		if (HittingWall || !NotAtEdge){
 			MoveRight = !MoveRight;
 		}
 
 		if (MoveRight){
 			transform.localScale = new Vector3(-0.25f,0.25f,1f);
-			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody>().velocity.y);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}
 		else {
 			transform.localScale = new Vector3(0.25f,0.25f,1f);
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody>().velocity.y);	
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);	
 		}
 	}
 }
